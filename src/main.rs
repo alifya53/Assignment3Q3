577310295a2578014bd521c0117901854b5624ee
Alifya53 <alifyahussain53@gmail.com>
2020-04-25 01:06:19 +0500

    code made successfully

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused_variables)]
+use {
+    hyper::{
+        // Following functions are used by Hyper to handle a `Request`
+        // and returning a `Response` in an asynchronous manner by using a Future
+        service::{make_service_fn, service_fn},
+        // Miscellaneous types from Hyper for working with HTTP.
+        Body,
+        Client,
+        Request,
+        Response,
+        Server,
+        Uri,
+    },
+    std::net::SocketAddr,
+};
+async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    // Always return successfully with a response containing a body with
+    // a friendly greeting ;)
+    Ok(Response::new(Body::from("hello, world! this is the first async program")))
 }
+  
+
+async fn run_server(addr: SocketAddr) {
+    println!("Listening on http://{}", addr);
+
+    
+    // Create a server bound on the provided address
+    let serve_future = Server::bind(&addr)
+        // Serve requests using our `async serve_req` function.
+        // `serve` takes a closure which returns a type implementing the
+        // `Service` trait. `service_fn` returns a value implementing the
+        // `Service` trait, and accepts a closure which goes from request
+        // to a future of the response.
+        .serve(make_service_fn(|_| {
+            async {
+                {
+                    Ok::<_, hyper::Error>(service_fn(serve_req))
+                }
+            }
+        }));
+
+    // Wait for the server to complete serving or exit with an error.
+    // If an error occurred, print it to stderr.
+    if let Err(e) = serve_future.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+  // Set the address to run our socket on.
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+  // Call our `run_server` function, which returns a future.
+  // As with every `async fn`, for `run_server` to do anything,
+  // the returned future needs to be run using `await`;
+  run_server(addr).await;
+}
+
+
+
+
